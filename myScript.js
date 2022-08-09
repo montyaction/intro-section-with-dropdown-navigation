82d3b5b59e2a1cf914a4533ea29ed1306529300a
spider-Monty <montyaction@hotmail.com>
2022-08-09 16:00:51 +0530

    Dropdwon submenu complete

--- myScript.js
+++ myScript.js
@@ -2,6 +2,11 @@ const navToggle = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.nav');
 const hamburger = document.querySelector('.hamburger');
 
+const navTooltips = document.querySelectorAll('.nav__tooltip');
+const navTooltipList = document.querySelectorAll('.nav__tooltip--list');
+console.log(navTooltips);
+console.log(navTooltipList);
+
 navToggle.addEventListener('click', () => {
 
     if (nav.classList.contains("nav__hidden")) {
@@ -15,4 +20,16 @@ navToggle.addEventListener('click', () => {
       hamburger.classList.remove("hamburger--close");
 
     }
-})
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunc1() {
+  document.querySelector("#myDropdown-1").classList.toggle("show-1");
+  document.querySelector("#arrow-dwon-1").classList.toggle("arrow--up");
+}
+
+function myFunc2() {
+  document.querySelector("#myDropdown-2").classList.toggle("show-2");
+  document.querySelector("#arrow-dwon-2").classList.toggle("arrow--up");
+}
